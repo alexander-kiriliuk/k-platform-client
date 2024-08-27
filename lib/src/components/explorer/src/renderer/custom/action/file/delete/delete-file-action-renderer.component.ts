1b5e5b38e1bf4a812aeb94c912396f17a2c1318a
Alexander <0074.sanu@gmail.com>
2024-08-28 00:11:27 +0300

    Fix build and publish config & refactoring

--- lib/src/components/explorer/src/renderer/custom/action/file/delete/delete-file-action-renderer.component.ts
+++ lib/src/components/explorer/src/renderer/custom/action/file/delete/delete-file-action-renderer.component.ts
@@ -33,7 +33,7 @@ import {Explorer} from "../../../../../../../explorer";
 import {ToastData, ToastEvent} from "../../../../../../../../global/vars";
 import {usePreloader} from "../../../../../../../../modules/preloader/src/use-preloader";
 import {FormControl, FormGroup} from "@angular/forms";
-import {File} from "@k-platform/client";
+import {File} from "../../../../../../../../modules/file";
 
 /**
  * This component provides functionality to confirm and delete a file.
